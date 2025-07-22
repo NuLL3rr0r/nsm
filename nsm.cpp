ccc7ac4189743d4ff064afb4603b56553ec23a29
Mamadou Babaei <info@babaei.net>
2025-07-22 22:25:43 +0200

    fix noParams overlapping comparisons always evaluate to false

--- nsm.cpp
+++ nsm.cpp
@@ -1452,7 +1452,7 @@ int main(int argc, const char* argv[])
 		else if(cmd == "watch")
 		{
 			//ensures correct number of parameters given
-			if(noParams < 2 && noParams > 5)
+			if(noParams < 2 || noParams > 5)
 				return parError(noParams, argv, "2-5");
 
 			WatchList wl;
